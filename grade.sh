ab24c1f4e2e1bf51260ffdbcd208c98785777869
AntDaKnee <akgonzalez@ucsd.edu>
2022-02-10 19:39:25 -0800

    added test to see if markdownparse is in file

--- grade.sh
+++ grade.sh
@@ -1,5 +1,11 @@
 
 git clone $1
+if [ -e MarkdownParse.java ]
+then
+    echo "ok"
+else
+    echo "nok"
+fi
 cp GradingTests.java CSE15L-Platypus/
 cd CSE15L-Platypus
 javac -cp lib/*:. GradingTests.java MarkdownParse.java
