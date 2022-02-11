19c7d4011a9f17eae6bc9b280a06b65390bfa9f5
AntDaKnee <akgonzalez@ucsd.edu>
2022-02-10 19:48:34 -0800

    added check for lib directory

--- grade.sh
+++ grade.sh
@@ -1,12 +1,17 @@
 
 git clone $1
-if [ -e MarkdownParse.java ]
-then
-    echo "ok"
-else
-    echo "nok"
-fi
 cp GradingTests.java CSE15L-Platypus/
 cd CSE15L-Platypus
+if [ -e MarkdownParse.java ]
+then
+    echo "Found MarkdownParse.java"
+else
+    echo "Did not find MarkdownParse.java"
+fi
+
+if [ ! -d "$lib" ]
+then
+    echo "No lib directory dummy"
+fi
 javac -cp lib/*:. GradingTests.java MarkdownParse.java
 java -cp lib/*:. org.junit.runner.JUnitCore GradingTests
